980d6883764bdea37913d286b3ce20935aad7911
Mohit Sati <146699961+SuperexMack@users.noreply.github.com>
2023-10-29 22:47:50 +0530

    Add files via upload

---
+++ pairSum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> pairSum(vector<int> &arr, int s)
+{
+    sort(arr.begin(), arr.end());
+
+    int sizo = arr.size();
+
+    vector<vector<int>> ans;
+    ;
+
+    for (int i = 0; i < sizo; i++)
+    {
+
+        for (int j = i + 1; j < sizo; j++)
+        {
+
+            if (arr[i] + arr[j] == s)
+            {
+
+                vector<int> sec;
+                sec.push_back(arr[i]);
+                sec.push_back(arr[j]);
+                ans.push_back(sec);
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> arr = {2, -6, 2, 5, 2};
+    pairSum(arr, 4);
+}
